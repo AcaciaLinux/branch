88457008bc75750bf34f2e13b061cdc8a6fa1b1c
zimsneexh <zimsneexh@gmail.com>
2022-08-25 21:00:11 +0200

    leafpkg.py: Drop sftp upload_package code

--- branchbuildbot/src/package/leafpkg.py
+++ branchbuildbot/src/package/leafpkg.py
@@ -2,8 +2,6 @@ import tarfile
 import os
 
 from log import blog
-import pysftp
-from paramiko import AuthenticationException
 
 class leafpkg():
     def __init__(self):
@@ -88,75 +86,3 @@ def create_tar_package(package_directory, package):
     pkg_file_tar.close()
     blog.info("Package file created in {}".format(package_directory))
     return os.path.join(package_directory, tar_name)
-
-def upload_package(package_path, package_build):
-    #sftp
-    blog.info("Connecting to repository server..")
-    try:
-        # TODO: config
-        sftp_con = pysftp.Connection(host="s.zsxh.eu", username="root", private_key="~/.ssh/id_rsa", private_key_pass="")
-    except AuthenticationException:
-        blog.error("Could not connect to the SSH Server. Authentication Failure.")
-        return "SSH_AUTH_FAILURE"
-
-    # TODO: config
-    #blog.info("Changing remote workdir to {}..".format(options.sftp_workdir))
-    sftp_con.cwd("/var/www/html/packages/")
-
-    blog.info("Fetching current package list..")
-    sftp_con.get("leaf.pkglist", localpath="/tmp/leaf.pkglist_temp")
-
-    blog.info("Updating package list..")
-    updatePkgList(package_build)
-
-    blog.info("Uploading updated package list..")
-    sftp_con.put("/tmp/leaf.pkglist")
-
-    blog.info("Cleaning up...")
-    os.remove("/tmp/leaf.pkglist")
-    os.remove("/tmp/leaf.pkglist_temp")
-
-    blog.info("Creating pkg subdir..")
-    try:
-        sftp_con.mkdir(package_build.name)
-    except IOError:
-        blog.warn("Package subdirectory already exists. Assuming update.")
-
-    sftp_con.cwd(package_build.name)
-
-    blog.info("Uploading package file..")
-    sftp_con.put(package_path)
-
-    sftp_con.close()
-    blog.info("Done uploading package file!")
-
-
-def updatePkgList(pkg_target):
-    blog.info("Parsing current package list..")
-
-    # old pkglist file
-    lfpkglist_file_old = open("/tmp/leaf.pkglist_temp", "r")
-    lfpkglist_arr_old = lfpkglist_file_old.read().split("\n")
-
-    # new pkglist file
-    lfpkglist_file_new = open("/tmp/leaf.pkglist", "w")
-
-    for prop in lfpkglist_arr_old:
-        prop_arr = prop.split(";")
-
-        pkg_name = prop_arr[0]
-
-        if(pkg_name == pkg_target.name):
-            blog.info("Updating {} in pkglist...".format(pkg_target.name))
-        elif(not pkg_name == ""):
-            lfpkglist_file_new.write(prop)
-            lfpkglist_file_new.write("\n")
-
-    blog.info("Appending package to pkglist")
-    tar_name = "{}-{}.lfpkg".format(pkg_target.name, pkg_target.version)
-
-    # TODO: config
-    url = "http://{}/{}/{}/{}".format("s.zsxh.eu", "packages", pkg_target.name, tar_name)
-
-    lfpkglist_file_new.write("{};{};{};{};{}\n".format(pkg_target.name, pkg_target.version, pkg_target.description, pkg_target.dependencies, url))
-    lfpkglist_file_new.close()
