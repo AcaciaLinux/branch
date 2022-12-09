8c54785f10ff8d7810efb9c532d0cbc5293d61dc
zimsneexh <38594041+zimsneexh@users.noreply.github.com>
2022-12-09 12:17:11 +0100

    config.py: Properly handle '=' in value (#36)

--- branchbuildbot/src/main.py
+++ branchbuildbot/src/main.py
@@ -42,6 +42,10 @@ def main():
     # load config
     blog.info("Loading configuration file..")
     conf = config.branch_options()
+
+    # check for valid conf
+    if(not conf.init_completed):
+        return -1
      
     # init leafcore
     if(buildenv.init_leafcore() != 0):
