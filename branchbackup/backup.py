6e33ff9fab2cf58c6846f0d85a473fe498c5dffa
zimsneexh <zimsneexh@gmail.com>
2023-04-30 09:22:48 +0200

    Update backup.py for ProtoV2

--- branchbackup/backup.py
+++ branchbackup/backup.py
@@ -1,6 +1,7 @@
 import blog
 import json
 import branchclient
+from branchpacket import BranchRequest, BranchResponse, BranchStatus
 import sys
 import os
 import packagebuild
@@ -21,33 +22,30 @@ def main():
     clientname = sys.argv[3]
     authkey = sys.argv[4]
 
-    blog.info("Connecting to {}:{} using clientname {}".format(host, port, clientname))
+    blog.info(f"Connecting to {host}:{port} using clientname '{clientname}'")
     bc = branchclient.branchclient(host, int(port), clientname, authkey, "CONTROLLER")
     
     blog.info("Fetching available packages..")
-    managed_packagebuilds = json.loads(bc.send_recv_msg("MANAGED_PKGBUILDS"))
+    managed_packagebuilds: BranchResponse = bc.send_recv_msg(BranchRequest("GETMANAGEDPKGBUILDS", "")).payload
     
     curr_time = time.strftime("%d.%m.%g-%H:%M:%S", time.localtime())
-    
     target_dir = os.path.join(LAUNCHDIR, "backup-" + curr_time)
 
-    blog.info("Beginning export to {}...".format(target_dir))
+    blog.info(f"Beginning export to {target_dir}...")
     pkgbuilds = [ ]
 
     for pkgbuild_name in managed_packagebuilds:
         blog.info("Checking out: {}".format(pkgbuild_name))
-        resp = bc.send_recv_msg("CHECKOUT_PACKAGE {}".format(pkgbuild_name))
+        resp = bc.send_recv_msg(BranchRequest("CHECKOUT", pkgbuild_name))
         
-        # check if package is valid
-        if(resp == "INV_PKG_NAME"):
-            blog.error("Packagebuild {} could not be found.".format(pkgbuild_name))
-            return
+        match resp.statuscode:
 
-        if(resp == "INV_PKG"):
-            blog.error("Packagebuild {} is damaged and could not be checked out.".format(pkgbuild_name))
-            return
-        
-        pkgbuilds.append(packagebuild.package_build.from_json(resp))
+            case BranchStatus.OK:
+                pkgbuilds.append(packagebuild.package_build.from_dict(resp.payload))
+
+            case other:
+                blog.error(f"Packagebuild '{pkgbuild_name}' could not be checked out: {resp.payload}")
+                return
 
     blog.info("{} package builds fetched.".format(len(pkgbuilds)))
     blog.info("Saving packagebuilds to {}".format(target_dir))
@@ -69,6 +67,7 @@ def main():
             return
     
     blog.info("Export completed.")
+    bc.disconnect()
 
 
 
